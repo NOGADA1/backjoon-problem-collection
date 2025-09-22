edb70139a2fd66c7fb5cd7752b3d3d3856135b64
안성범 <168620337+NOGADA1@users.noreply.github.com>
2025-09-22 12:48:31 +0900

    Update backjoon2751.cs

--- backjoon2751.cs
+++ backjoon2751.cs
@@ -9,7 +9,7 @@ public class Backjoon2751
     public static void Main(string[] args)
     {
         int num = int.Parse(Console.ReadLine()); // 정렬할 수의 횟수를 입력받음
-        var list = new List<int>(); // 배열로 했다가 안돼서 리스트로 만들어봄
+        var list = new List<int>(); // 배열로 했다가 안돼서 리스트로 만들어봄, 다시 배열로 구현 해 본 결과 배열이 미세하게나마 더 효율적임 물리적 주소가 연결되어있기때문
         StringBuilder output = new StringBuilder(); // Console.WriteLine이 뒤지게 느려서 스트링 빌더로 한꺼번에 만들고 출력하기위해 스트링빌더 사용
         for (int i = 0; i < num; i++) // 리스트 입력
         {
